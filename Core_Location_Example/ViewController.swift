5ecffe0afc486160f518cb1e80bcf950d3e2f6fb
Sriteja <sriteja.csriteja@gmail.com>
2017-07-10 21:18:46 +0530

    Initial Commit

---
+++ Core_Location_Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core_Location_Example
+//
+//  Created by Sriteja Chilakamarri on 10/07/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
