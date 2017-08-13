beed0f71633f96f51256c3f8ae4213f2b35a8103
Sriteja <sriteja.csriteja@gmail.com>
2017-08-14 00:17:02 +0530

    Initial Commit

--- Core_Location_Example/ViewController.swift
+++ Core_Location_Example/ViewController.swift
@@ -7,18 +7,57 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        getUserLocation()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        
+        // Here we start locating
+        locationManager.startUpdatingLocation()
+    
+    }
+    
+    func getUserLocation(){
+        
+        //SVProgressHUD.show()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            switch(CLLocationManager.authorizationStatus()) {
+            case .notDetermined, .restricted, .denied:
+                print("No access")
+            case .authorizedAlways, .authorizedWhenInUse:
+                print("Access")
+                
+            }
+        } else {
+            print("Location services are not enabled")
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        //print(location.coordinate.latitude,location.coordinate.longitude)
+        
+        print(location.altitude,location.course,location.floor,location.speed,location.timestamp,location.verticalAccuracy)
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
